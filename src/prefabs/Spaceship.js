b0c365bdd05301397c6c1c6782e550e627ad3baa
wilsonccy1016 <cchu27@ucsc.edu>
2021-04-19 16:56:37 -0700

    initial commit

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -1,5 +1,3 @@
-const { Phaser } = require("../../lib/phaser");
-
 class Spaceship extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame, pointValue) {
         super(scene, x, y, texture, frame);
