da498d0d886d9bff00c3725001238cbcca256660
wilsonccy1016 <cchu27@ucsc.edu>
2021-04-21 23:30:35 -0700

    commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
-    scene: [ Menu, Play ]
+    scene: [ Menu, Play, Play2 ]
 }
 let game = new Phaser.Game(config);
 let borderUISize = game.config.height / 15;
