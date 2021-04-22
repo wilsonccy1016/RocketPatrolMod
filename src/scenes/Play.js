4552c8f62b7db5148a36f5e6aa5d8c35b46e66b7
wilsonccy1016 <cchu27@ucsc.edu>
2021-04-21 21:41:20 -0700

    commit

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,7 +6,7 @@ class Play extends Phaser.Scene {
   preload() {
       this.load.image('rocket', './assets/rocket.png');
       this.load.image('spaceship', './assets/spaceship.png');
-      this.load.image('birdie', './assests/birdie.png.');
+      this.load.image('birdie', './assets/birdie.png.');
       this.load.image('starfield', './assets/starfield.png');
       this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
   }
