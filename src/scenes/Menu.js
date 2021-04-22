3f17fa27c6c0a762703f0519866aabfcfaa25b3a
wilsonccy1016 <cchu27@ucsc.edu>
2021-04-21 19:43:11 -0700

    commit

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -24,7 +24,7 @@ class Menu extends Phaser.Scene {
     }
     this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
     this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
-    menuConfig.backgroundColor = '#fffb03';
+    menuConfig.backgroundColor = '#FFFB03';
     menuConfig.color = '#000';
     this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
 
